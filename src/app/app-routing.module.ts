1dd6fd995c4485f7655aeed5e93f82e2105e7b14
alberico fagundes <alberico@fagundes.live>
2023-05-15 02:04:50 -0300

    criado rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CadastroUserComponent } from './views/cadastro-user/cadastro-user.component';
+import { BlankTemplateComponent } from './shared/components/blank-template/blank-template.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BlankTemplateComponent,
+    children: [
+      {
+        path: 'register',
+        component: CadastroUserComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
